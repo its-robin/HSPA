6c946bcf7b2759cdb3548dd35d26b563154d509e
kherarobin84 <robinkhera@gmail.com>
2022-07-31 11:41:46 +0530

    First Commit SPA

---
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector : 'app-property-card',
+  templateUrl:'property-card.component.html',
+  styleUrls:['property-card.component.css']
+})
+export class PropertyCardComponent {
+Property: any ={
+  "Id":1,
+  "Name":"Robin House",
+  "Type":"House",
+  "Price":12000
+}
+}
